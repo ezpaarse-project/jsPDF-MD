8fd0269bbff663a5cfbb267d849a09bacfdcaa99
oxypomme <oxytom8@gmail.com>
2023-08-03 15:11:18 +0200

    ci: added semantic-release in GitHub action

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,6 +4,9 @@ module.exports = {
     node: true,
   },
   extends: ['airbnb-base', 'airbnb-typescript/base'],
+  ignorePatterns: [
+    '!.releaserc.js',
+  ],
   parserOptions: {
     ecmaVersion: 'latest',
     project: './tsconfig.eslint.json',
