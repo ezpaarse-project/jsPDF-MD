67de55232a69af919572865ce1fcc5e0a2882f42
oxypomme <oxytom8@gmail.com>
2025-07-16 08:19:30 +0200

    fix: fixed parser not beign exported anymore

--- src/index.ts
+++ src/index.ts
@@ -104,6 +104,10 @@ try {
 export default mdToPDF;
 export type {
   ImgRemoteRequestor as MdImgRemoteRequestor,
+  /** * @deprecated use `MdParser` instead  */
+  Parser,
+  Parser as MdParser,
+  /** * @deprecated use `MdParserOptions` instead  */
   PluginOptions,
   PluginOptions as MdParserOptions,
 };
